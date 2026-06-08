6471943ded3873ce69ca6a31c87db14a9b492433
UjjawalPratap1 <thakurups2324@gmail.com>
2026-06-09 00:08:01 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,13 +5,16 @@ const cors = require('cors');
 const app = express();
 const interviewRouter = require('./routes/interview.routes.js')
 
-app.use(express.json());
-app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173",
-    "https://interview-ai-2-kefk.onrender.com"
+    origin: [
+    "http://localhost:5173",
+    "https://interview-ai-front.onrender.com"
     ],credentials: true
 }))
+
+app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRouter);
 app.use("/api/interview", interviewRouter);
 
